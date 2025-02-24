fb551804865d1c5204ddf60cd0b004253e62ccb9
MatteoVentura <matteo.ventura18@gmail.com>
2025-02-24 16:03:04 +0100

    add middlewares

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const cors = require("cors");
+const { PORT, FE_URL } = process.env;
+// Middlewares
+const notFound = require("./middlewares/notFound");
+const errorsHandler = require("./middlewares/errorsHandler");
+// Router
+const moviesRouter = require("./routers/moviesRouter");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// middleware per i file statici
+app.use(express.static("public"));
+// middleware per il parsing del req.body
+app.use(express.json());
+// middleware CORS (che permette la comunicazione con il FE)
+app.use(
+  cors({
+    origin: FE_URL,
+  })
+);
+
+// Routes (le rotte della mia applicazione)
+app.use("/movies", moviesRouter);
+
+// Middlewares - Per la gestione degli errori (404, 500)
+app.use(notFound);
+app.use(errorsHandler);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
