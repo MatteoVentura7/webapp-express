1c0dee08b2901d54188274d046cd652e22d9960d
MatteoVentura <matteo.ventura18@gmail.com>
2025-02-24 16:24:24 +0100

    add router

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const movieController = require("../controllers/movieController");
+
+// La lista delle rotte con la funzione corrispondente del controller
+
+// Index
+router.get("/", movieController.index);
+
+// Show
+router.get("/:id", movieController.show);
+
+// Destroy
+router.delete("/:id", movieController.destroy);
 
 module.exports = router;
